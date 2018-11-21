ad0a1350cf82562e41c87198ab081e089b236ff2
Jakub <kuba.lanecki@gmail.com>
2018-11-21 17:26:04 +0100

    Initial commit.

---
+++ test/bibtex_search/MainTest.java
@@ -0,0 +1,12 @@
+package bibtex_search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
